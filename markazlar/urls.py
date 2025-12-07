e6885d00e211765452f46670913a69e1c86c1680
Jasmin <jasminnurmanova@webster.edu>
2025-12-07 23:08:57 +0500

    news

--- markazlar/urls.py
+++ markazlar/urls.py
@@ -1,9 +1,10 @@
-from .views import home_page,markazlar_func,detail_markazlar
+from .views import home_page,markazlar_func,detail_markazlar,create_markaz
 from django.urls import path
 
 urlpatterns=[
     path('',home_page,name='home_page'),
     path('markazlar/<int:pk>',markazlar_func,name='markazlar'),
-    path('details/<int:pk>', detail_markazlar, name='details')
+    path('details/<int:pk>', detail_markazlar, name='details'),
+    path('markaz_create', create_markaz, name='create')
 
 ]
--- markazlar/views.py
+++ markazlar/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Markazlar,Category
 
@@ -16,3 +17,24 @@ def detail_markazlar(request,pk):
     markazlar=Markazlar.objects.filter(pk=pk).first()
     print(111, markazlar)
     return render(request,'details.html',{'markazlar':markazlar})
+
+def create_markaz(request):
+    if request.method == ' POST':
+        name=request.POST.get('name','')
+        category=Category.objects.filter (name= request.POST.get('category','')).first(),
+        created_at=request.POST.get('created_at','')
+        location=request.POST.get('name','')
+        desc=request.POST.get('desc','')
+        image=request.FILES.get('image')
+
+        markaz=Markazlar(
+            name=name,
+            category=category,
+            created_at=created_at,
+            location=location,
+            desc=desc,
+            image=image
+        )
+        return redirect('details', markaz.pk)
+
+    return render(request,'create_markaz.html')
